b00d22751dd4503142efe7e0be02db3aa2d4409c
Christoper <c.blake@hotmail.se>
2021-08-18 15:09:06 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GIT_MVC_GROUP6B.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
